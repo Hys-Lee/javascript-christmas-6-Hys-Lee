fbe3c5d4e12f87c0985d1fc921e80aba7f241592
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-12 23:46:38 +0900

    ✨Feat(EventsModel): add condition checking
    add checkers for dependant contitions on features.

--- src/models/domain/EventsModel.js
+++ src/models/domain/EventsModel.js
@@ -1,4 +1,5 @@
 import { MENULIST } from '../constants/MenuData';
+import DayOfTheWeek from '../constants/DayOfTheWeek';
 
 class EventsModel {
   /**
@@ -19,30 +20,62 @@ class EventsModel {
 
   constructor(dateInfo, order) {
     this.#day = dateInfo.day;
-    this.#dayOfTheWeek = dateInfo.dateInfo;
+    this.#dayOfTheWeek = dateInfo.dayOfTheWeek;
     this.#menuCount = order.menuCount;
     this.#payment = order.payment;
     this.#specialDiscount = 1_000;
     this.#giftPrice = MENULIST.drink.champagne;
   }
 
-  calculateDDayDiscount() {
+  checkDDayDiscount() {
+    return this.#day <= 25;
+  }
+
+  resultDDayDiscount() {
+    if (!this.checkDDayDiscount()) return 0;
     return (this.#day - 1) * 100 + 1_000;
   }
 
-  calculateWeekdayDiscount() {
+  checkWeekdayDiscount() {
+    const dayOfTheWeekCondition =
+      this.#dayOfTheWeek !== DayOfTheWeek.FRIDAY ||
+      this.#dayOfTheWeek !== DayOfTheWeek.SATURDAY;
+    const menuCondition = this.#menuCount.desert > 0;
+    return dayOfTheWeekCondition && menuCondition;
+  }
+
+  resultWeekdayDiscount() {
+    if (!this.checkWeekdayDiscount()) return 0;
     return this.#menuCount.desert * 2_023;
   }
 
-  calculateWeekendDiscount() {
+  checkWeekendDiscount() {
+    const dayOfTheWeekCondition =
+      this.#dayOfTheWeek === DayOfTheWeek.FRIDAY ||
+      this.#dayOfTheWeek === DayOfTheWeek.SATURDAY;
+    const menuCondition = this.#menuCount.main > 0;
+    return dayOfTheWeekCondition && menuCondition;
+  }
+
+  resultWeekendDiscount() {
+    if (!this.checkWeekendDiscount()) return 0;
     return this.#menuCount.main * 2_023;
   }
 
-  calculateSpecialDiscount() {
+  checkSpecialDiscount() {
+    const dayOfTheWeekCondition = this.#dayOfTheWeek === DayOfTheWeek.SUNDAY;
+    const CHRISTMAS_DAY = 25;
+    return this.#day === CHRISTMAS_DAY || dayOfTheWeekCondition;
+  }
+
+  resultSpecialDiscount() {
+    if (!this.checkSpecialDiscount()) return 0;
     return this.#specialDiscount;
   }
 
-  calculateGiftPrice() {
+  // totalPayment는 planner의 정보. 할인 전 총주문 금액
+  resultGiftPrice(totalPayment) {
+    if (totalPayment < 120_000) return 0;
     return this.#giftPrice;
   }
 }
