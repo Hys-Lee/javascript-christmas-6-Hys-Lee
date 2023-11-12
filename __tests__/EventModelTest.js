67e440a3b5d62e88c9264ef37c875768915d1ee5
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-12 20:34:11 +0900

    ✅Test(EventsModel): test weekend discount
    test for calculating weekend discounting price.
    style to rename EventsModel.
    update docs.

--- __tests__/EventModelTest.js
+++ __tests__/EventModelTest.js
@@ -1,11 +1,11 @@
-import EventsModels from '../src/models/domain/EventsModel.js';
+import EventsModel from '../src/models/domain/EventsModel.js';
 import DayOfTheWeek from '../src/models/constants/DayOfTheWeek.js';
 
 describe('EventModel 테스트', () => {
   test('D-day 할인 테스트', () => {
     const dateInfo = { day: 2, dayOfTheWeek: DayOfTheWeek.SATURDAY };
     const order = { menuCount: { desert: 1, main: 1 }, payment: 10_000 };
-    const eventsModel = new EventsModels(dateInfo, order);
+    const eventsModel = new EventsModel(dateInfo, order);
     const result = eventsModel.calculateDDayDiscount();
     const output = 1_100;
     expect(result).toBe(output);
@@ -14,9 +14,17 @@ describe('EventModel 테스트', () => {
   test('평일 할인 테스트', () => {
     const dateInfo = { day: 4, dayOfTheWeek: DayOfTheWeek.MONDAY };
     const order = { menuCount: { desert: 2, main: 3 }, payment: 10_000 };
-    const eventsModel = new EventsModels(dateInfo, order);
+    const eventsModel = new EventsModel(dateInfo, order);
     const result = eventsModel.calculateWeekdayDiscount();
     const output = 2_023 * 2;
+    expect(result).toBe(output);
+  });
+  test('주말 할인 테스트', () => {
+    const dateInfo = { day: 4, dayOfTheWeek: DayOfTheWeek.MONDAY };
+    const order = { menuCount: { desert: 2, main: 3 }, payment: 10_000 };
+    const eventsModel = new EventsModel(dateInfo, order);
+    const result = eventsModel.calculateWeekendDiscount();
+    const output = 2_023 * 3;
     expect(result).toBe(output);
   });
 });
