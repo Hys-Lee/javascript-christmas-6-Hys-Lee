ab57910072f0c3a3be61e976851c193b1f25e417
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-12 20:30:01 +0900

    ✨Feat(EventsModel): make weekday discount
    pass the test.

--- src/models/domain/EventsModel.js
+++ src/models/domain/EventsModel.js
@@ -1,22 +1,29 @@
 class EventsModel {
   /**
    * @param {day, dayOfTheWeek} dateInfo
-   *
+   * @param { menuCount: { desert, main }, payment } order
    */
   #day;
 
   #dayOfTheWeek;
 
-  #order;
+  #menuCount;
+
+  #payment;
 
   constructor(dateInfo, order) {
     this.#day = dateInfo.day;
     this.#dayOfTheWeek = dateInfo.dateInfo;
-    this.#order = order;
+    this.#menuCount = order.menuCount;
+    this.#payment = order.payment;
   }
 
   calculateDDayDiscount() {
     return (this.#day - 1) * 100 + 1_000;
+  }
+
+  calculateWeekdayDiscount() {
+    return this.#menuCount.desert * 2_023;
   }
 }
 
