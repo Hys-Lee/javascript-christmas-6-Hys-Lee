b24675115930d27e4ed70caaf8e60932d14f6a05
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-12 20:27:33 +0900

    ✅Test(EventsModel): test weekday discount
    test for calculating weekday discounting price.
    style to rename class name and its instance.

--- __tests__/EventModelTest.js
+++ __tests__/EventModelTest.js
@@ -1,13 +1,22 @@
-import EventModels from '../src/models/domain/EventModel';
-import DayOfTheWeek from '../src/models/constants/DayOfTheWeek';
+import EventsModels from '../src/models/domain/EventsModel.js';
+import DayOfTheWeek from '../src/models/constants/DayOfTheWeek.js';
 
 describe('EventModel 테스트', () => {
   test('D-day 할인 테스트', () => {
     const dateInfo = { day: 2, dayOfTheWeek: DayOfTheWeek.SATURDAY };
     const order = { menuCount: { desert: 1, main: 1 }, payment: 10_000 };
-    const eventModel = new EventModels(dateInfo, order);
-    const result = eventModel.calculateDDayDiscount();
+    const eventsModel = new EventsModels(dateInfo, order);
+    const result = eventsModel.calculateDDayDiscount();
     const output = 1_100;
+    expect(result).toBe(output);
+  });
+
+  test('평일 할인 테스트', () => {
+    const dateInfo = { day: 4, dayOfTheWeek: DayOfTheWeek.MONDAY };
+    const order = { menuCount: { desert: 2, main: 3 }, payment: 10_000 };
+    const eventsModel = new EventsModels(dateInfo, order);
+    const result = eventsModel.calculateWeekdayDiscount();
+    const output = 2_023 * 2;
     expect(result).toBe(output);
   });
 });
