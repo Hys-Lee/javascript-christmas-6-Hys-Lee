d2f541dc1f790c877af38dcf60bb97e36a24f829
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-14 14:37:14 +0900

    ✅Test(EventPlanner): test calculating all benefits
    test for calculating total benefits money
    including gift price.

--- __tests__/EventPlannerTest.js
+++ __tests__/EventPlannerTest.js
@@ -10,6 +10,7 @@ describe('EventPlanner 테스트', () => {
     drink: { zeroCola: 1, redWine: 1, champagne: 0 },
   };
   const planner = new EventPlanner(dateInfo, rawOrder);
+  const totalPayment = 6_000 + 55_000 + 5_000 + 3_000 + 60_000;
   test('주문한 메뉴 타입 분류 및 카운팅', () => {
     const result = EventPlanner.countMenuType(rawOrder);
     const output = { appetizer: 1, main: 1, desert: 1, drink: 2 };
@@ -17,19 +18,25 @@ describe('EventPlanner 테스트', () => {
   });
   test('할인 전 총주문 금액 계산', () => {
     const result = EventPlanner.calculateTotalPayment(rawOrder);
-    const output = 6_000 + 55_000 + 5_000 + 3_000 + 60_000;
-    expect(result).toEqual(output);
+    const output = totalPayment;
+    expect(result).toBe(output);
   });
   test('적용 가능 혜택 체크', () => {
-    const result = planner.summaryBenefitsResult(10_000);
+    const result = planner.summaryBenefitsResult(totalPayment);
     const output = {
       discountPrice: { dDay: 1_100, weekday: 0, weekend: 2_023, special: 0 },
-      giftPrice: 0,
+      giftPrice: 25_000,
     };
 
     expect(result).toEqual(output);
   });
 
+  test('총혜택 금액 계산', () => {
+    const benefitsResult = planner.summaryBenefitsResult(totalPayment);
+    const result = EventPlanner.calculateTotalBenefits(benefitsResult);
+    const output = 1_100 + 2_023 + 25_000;
+    expect(result).toBe(output);
+  });
   test('혜택 내역 산출', () => {
     const beneftis = planner.summaryBenefitsResult(10_000);
     const result = EventPlanner.formApplyingBenefitsList(beneftis);
