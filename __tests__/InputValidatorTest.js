a717f40dd015850529e72c201cce31820db2a6a9
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-15 13:53:38 +0900

    ✅Test(InputValidaor): test for drink only order
    test for forbidden drink only order using this function .

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -11,4 +11,16 @@ describe(`InputValidator 테스트`, () => {
     const output = true;
     expect(result).toBe(output);
   });
+  test('음료만 주문 체크', () => {
+    const totalOrder = {
+      appetizer: { buttonMushroomSoup: 0, tapas: 0, caesarSalad: 0 },
+      main: { tBoneSteak: 0, bBQRibs: 0, seafoodPasta: 0, christmasPasta: 0 },
+      desert: { chocolateCake: 0, iceCream: 0 },
+      drink: { zeroCola: 1, redWine: 1, champagne: 0 },
+    };
+    const validator = new InputValidator(totalOrder);
+    const result = validator.hasOnlyDrink();
+    const output = true;
+    expect(result).toBe(output);
+  });
 });
