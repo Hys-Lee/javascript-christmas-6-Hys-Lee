d8ae8d9a8ab0f9cd66cbeef3ef05da1b77bb589e
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-15 15:00:52 +0900

    ✅Test(InputValidaor): test on menu more than 20
    test not to order more than 20.

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -34,4 +34,16 @@ describe(`InputValidator 테스트`, () => {
     const output = true;
     expect(result).toBe(output);
   });
+  test(`20개 이하 주문 체크`, () => {
+    const totalOrder = {
+      appetizer: { buttonMushroomSoup: 0, tapas: 0, caesarSalad: 0 },
+      main: { tBoneSteak: 20, bBQRibs: 0, seafoodPasta: 0, christmasPasta: 0 },
+      desert: { chocolateCake: 0, iceCream: 0 },
+      drink: { zeroCola: 1, redWine: 1, champagne: 0 },
+    };
+    const validator = new InputValidator(totalOrder);
+    const result = validator.hasLargerThanTwenty();
+    const output = true;
+    expect(result).toBe(output);
+  });
 });
